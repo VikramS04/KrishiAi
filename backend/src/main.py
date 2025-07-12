dac916e323020f14418d7d91bce2bc113c524788
Vikram Saini <138091839+VikramS04@users.noreply.github.com>
2025-07-12 23:31:12 +0530

    Update  removed imoport block main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -116,9 +116,9 @@ def api_docs():
 #if __name__ == '__main__':
   #  app.run(host='0.0.0.0', port=5001, debug=True) 
 
-import os
+# import os
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
